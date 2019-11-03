3713fe465025061ed7ec72706c2a7e9b2f6c7e29
kikadf <kikadf.01@gmail.com>
2019-11-03 16:18:56 +0100

    Add plex-media-server to version check
    Check AUR instead of upstream

--- tools/version.sh
+++ tools/version.sh
@@ -8,6 +8,8 @@ _WATCH_5=$(lynx -dump -listonly "https://github.com/meganz/MEGAcmd/tags" | sed -
 #_WATCH_6=$(lynx -dump "https://www.cyberpowersystems.com/product/software/powerpanel-for-linux/" | sed -n 's|.*Current version: \(.*\)|\1|p')
 _WATCH_7a=$(lynx -dump -listonly "https://github.com/networkupstools/nut/tags" | sed -n 's:.*v\([0-9].*\)\.tar\.gz:\1:p' | head -1)
 _WATCH_7b=$(lynx -dump "https://github.com/networkupstools/nut" | sed -n 's:.*\[[0-9]*\]\([0-9].*\),\([0-9]*\) commits.*:\1\2:p')
+_WATCH_8=$(lynx -dump -listonly "https://aur.archlinux.org/packages/plex-media-server/" | sed -n 's|.*plexmediaserver-\([0-9].*\)-.*.rpm.*|\1|p')
+
 
 #echo "mantisbt=$_WATCH_1"
 #echo "linux-armv7-lts=$_WATCH_2"
@@ -16,6 +18,7 @@ echo "mega-sdk=$_WATCH_4"
 echo "mega-cmd=$_WATCH_5"
 #echo "powerpanel=$_WATCH_6"
 echo "network-ups-tools=$_WATCH_7a.$_WATCH_7b"
+echo "plex-media-server=$_WATCH_8"
 
 exit 0
 
