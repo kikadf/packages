808647495574800f690ec43a79de937dc4755d46
kikadf <kikadf.01@gmail.com>
2020-09-28 08:53:49 +0200

    Add _Linux ext to version check of megacmd

--- tools/makepkg-versions.sh
+++ tools/makepkg-versions.sh
@@ -4,7 +4,7 @@
 #_WATCH_2=$(lynx -dump -listonly "https://www.kernel.org/" | sed -n 's|.*linux-\(4\.9\.[0-9]*\).tar.xz|\1|p')
 _WATCH_3=$(lynx -dump "https://www.denx.de/wiki/U-Boot/ReleaseCycle" | sed -n 's:.*U-Boot v\([0-9].*\) was.*:\1:p' | head -1)
 #_WATCH_4=$(lynx -dump -listonly "https://github.com/meganz/sdk/tags" | sed -n 's:.*v\([0-9].*\)\.tar\.gz:\1:p' | head -1)
-_WATCH_5=$(lynx -dump -listonly "https://github.com/meganz/MEGAcmd/tags" | sed -n 's:.*archive.\(.*[0-9]\).*\.tar\.gz:\1:p' | head -1)
+_WATCH_5=$(lynx -dump -listonly "https://github.com/meganz/MEGAcmd/tags" | sed -n 's:.*archive.\(.*[0-9]\)_Linux.*\.tar\.gz:\1:p' | head -1)
 #_WATCH_6=$(lynx -dump "https://www.cyberpowersystems.com/product/software/powerpanel-for-linux/" | sed -n 's|.*Current version: \(.*\)|\1|p')
 #_WATCH_7a=$(lynx -dump -listonly "https://github.com/networkupstools/nut/tags" | sed -n 's:.*v\([0-9].*\)\.tar\.gz:\1:p' | head -1)
 #_WATCH_7b=$(lynx -dump "https://github.com/networkupstools/nut" | sed -n 's:.*\[[0-9]*\]\([0-9].*\),\([0-9]*\) commits.*:\1\2:p')
