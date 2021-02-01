ce8c93050ee21497d23e2b81c6421a948322166c
kikadf <kikadf.01@gmail.com>
2021-02-01 13:18:26 +0100

    pkgsrc-versions.sh: add krusader-kf5, refactoring

--- tools/pkgsrc-versions.sh
+++ tools/pkgsrc-versions.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+_get_pkgsrcver() {
+	lynx -dump "https://raw.githubusercontent.com/NetBSD/pkgsrc-wip/master/$1/Makefile" | sed -n "s|^DISTNAME.*-\([0-9].*\)|\1|p"
+}
+
+_get_ever() {
+	lynx -dump -listonly "https://enlightenment.org/download" | sed -n "s|.*/$1-\([0-9].*\)\.tar\.xz.*|\1|p"
+}
 
 _get_versions() {
-	_PKGSRCVER_1=$(lynx -dump "https://raw.githubusercontent.com/NetBSD/pkgsrc-wip/master/efl/Makefile" | sed -n 's|.*efl-\([0-9].*\)|\1|p')
-	_REALVER_1=$(lynx -dump -listonly "https://enlightenment.org/download" | sed -n 's|.*/efl-\([0-9].*\)\.tar\.xz.*|\1|p')
-	echo "efl=$_PKGSRCVER_1=$_REALVER_1"
-	_PKGSRCVER_2=$(lynx -dump "https://raw.githubusercontent.com/NetBSD/pkgsrc-wip/master/enlightenment-current/Makefile" | sed -n 's|.*enlightenment-\([0-9].*\)|\1|p')
-	_REALVER_2=$(lynx -dump -listonly "https://enlightenment.org/download" | sed -n 's|.*/enlightenment-\([0-9].*\)\.tar\.xz.*|\1|p')
-	echo "enlightenment-current=$_PKGSRCVER_2=$_REALVER_2"
-	_PKGSRCVER_3=$(lynx -dump "https://raw.githubusercontent.com/NetBSD/pkgsrc-wip/master/terminology/Makefile" | sed -n 's|.*terminology-\([0-9].*\)|\1|p')
-	_REALVER_3=$(lynx -dump -listonly "https://enlightenment.org/download" | sed -n 's|.*/terminology-\([0-9].*\)\.tar\.xz.*|\1|p')
-	echo "terminology=$_PKGSRCVER_3=$_REALVER_3"
+	echo "efl=$(_get_pkgsrcver efl)=$(_get_ever efl)"
+	echo "enlightenment-current=$(_get_pkgsrcver enlightenment-current)=$(_get_ever enlightenment)"
+	echo "terminology=$(_get_pkgsrcver terminology)=$(_get_ever terminology)"
+	echo "krusader-kf5=$(_get_pkgsrcver krusader-kf5)=$(lynx -dump "https://download.kde.org/stable/krusader/" | sed -n "s|.*krusader/\([0-9].*\)/|\1|p" | tail -1)"
 }
 
 _START=$(date +"%Y-%m-%d/%H:%M:%S")
 
 for i in $(_get_versions); do
 	_name=$(echo $i | cut -d = -f 1)
-	_aurver=$(echo $i | cut -d = -f 2)
+	_pkgsrcver=$(echo $i | cut -d = -f 2)
 	_realver=$(echo $i | cut -d = -f 3)
-	if [ "$_aurver" = "$_realver" ]; then
+	if [ "$_pkgsrcver" = "$_realver" ]; then
 		echo "$_name is up to date"
 	else
-		echo "$_name: $_aurver != $_realver"
+		echo "$_name: $_pkgsrcver != $_realver"
 	fi
 done
 
