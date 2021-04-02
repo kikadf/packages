e671c25af99d77fce4ed60e35476bd71d04512cf
kikadf <kikadf.01@gmail.com>
2021-04-02 17:27:52 +0200

    Fix makepkg-versions.sh: mega-cmd version check

--- tools/makepkg-versions.sh
+++ tools/makepkg-versions.sh
@@ -4,7 +4,7 @@
 #echo "linux-armv7-lts=$(lynx -dump -listonly "https://www.kernel.org/" | sed -n 's|.*linux-\(4\.9\.[0-9]*\).tar.xz|\1|p')"
 echo "uboot-opipc=$(lynx -dump "https://www.denx.de/wiki/U-Boot/ReleaseCycle" | sed -n 's:.*U-Boot v\([0-9].*\) was.*:\1:p' | head -1)"
 #echo "mega-sdk=$(lynx -dump -listonly "https://github.com/meganz/sdk/tags" | sed -n 's:.*v\([0-9].*\)\.tar\.gz:\1:p' | head -1)"
-echo "mega-cmd=$(lynx -dump -listonly "https://github.com/meganz/MEGAcmd/tags" | sed -n 's:.*archive.\(.*[0-9]\)_Linux.*\.tar\.gz:\1:p' | head -1)"
+echo "mega-cmd=$(lynx -dump -listonly "https://github.com/meganz/MEGAcmd/tags" | sed -n 's:.*tags.\(.*[0-9]\)_Linux.*\.tar\.gz:\1:p' | head -1)"
 #echo "powerpanel=$(lynx -dump "https://www.cyberpowersystems.com/product/software/powerpanel-for-linux/" | sed -n 's|.*Current version: \(.*\)|\1|p')"
 #_WATCH_7a=$(lynx -dump -listonly "https://github.com/networkupstools/nut/tags" | sed -n 's:.*v\([0-9].*\)\.tar\.gz:\1:p' | head -1)
 #_WATCH_7b=$(lynx -dump "https://github.com/networkupstools/nut" | sed -n 's:.*\[[0-9]*\]\([0-9].*\),\([0-9]*\) commits.*:\1\2:p')
